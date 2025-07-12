2277ff40c41e63bf4c84b126647f3c9ed13ac419
Sifiso Ntuli <sifisontulisn88@gmail.com>
2025-07-12 11:05:09 +0200

    correction commit

--- alx-react-app/src/components/WelcomeMessage.jsx
+++ alx-react-app/src/components/WelcomeMessage.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function WelcomeMessage() {
   return (
     <div>
-      <h1>Hello everyone, I am learning React at ALZ!</h1>
+      <h1>Hello everyone, I am learning React at ALX!</h1>
       <p>I am learning about JSX!</p>
     </div>
   )
