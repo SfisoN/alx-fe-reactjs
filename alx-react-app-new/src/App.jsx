215b7f33d7fb3e04a0200e5b46b2c5b3fb2ea4f4
Sifiso Ntuli <sifisontulisn88@gmail.com>
2025-07-17 15:38:21 +0200

    updated commit

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -3,14 +3,13 @@ import Header from "./components/Header";
 import MainContent from "./components/MainContent";
 import Footer from "./components/Footer";
 import UserProfile from "./components/UserProfile";
+import Counter from "./components/Counter";
 
 function App() {
 
   return (
     <>
-      <Header />
-      <MainContent /> 
-      <Footer />
+      <Counter />
     </>
    
     
