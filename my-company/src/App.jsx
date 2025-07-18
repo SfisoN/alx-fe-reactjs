f1558ab1c2407e35e75d41ce8373889c8d47c393
Sifiso Ntuli <sifisontulisn88@gmail.com>
2025-07-18 13:43:23 +0200

    updated commit

--- my-company/src/App.jsx
+++ my-company/src/App.jsx
@@ -1,8 +1,8 @@
-import Navbar from "./Navbar";
-import Home from "./Home";
-import About from "./About";
-import Services from "./Services";
-import Contact from "./Contact";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import About from "./components/About";
+import Services from "./components/Services";
+import Contact from "./components/Contact";
 import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
 
 function App() {
