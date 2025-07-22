a8ad2e2324d68ee517df182b40ea3369919b4b60
Sifiso Ntuli <sifisontulisn88@gmail.com>
2025-07-22 16:53:59 +0200

    new commit

--- recipe-sharing-app/src/components/AddRecipeForm.jsx
+++ recipe-sharing-app/src/components/AddRecipeForm.jsx
@@ -1,21 +1,6 @@
 import { useState } from 'react';
 import { useRecipeStore } from './recipeStore';
 
-// RecipeList component
-const RecipeList = () => {
-  const recipes = useRecipeStore(state => state.recipes);
-
-  return (
-    <div>
-      {recipes.map(recipe => (
-        <div key={recipe.id}>
-          <h3>{recipe.title}</h3>
-          <p>{recipe.description}</p>
-        </div>
-      ))}
-    </div>
-  );
-};
 
 // AddRecipeForm component
 const AddRecipeForm = () => {
@@ -48,4 +33,4 @@ const AddRecipeForm = () => {
   );
 };
 
-export { RecipeList, AddRecipeForm };
+export default AddRecipeForm;
--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -1,10 +1,20 @@
-import create from 'zustand';
+import { useRecipeStore } from './recipeStore';
 
-const useRecipeStore = create(set => ({
-  recipes: [],
-  addRecipe: (newRecipe) => set(state => ({ recipes: [...state.recipes, newRecipe] })),
-  setRecipes: (recipes) => set({ recipes })
-}));
+// RecipeList component
+const RecipeList = () => {
+  const recipes = useRecipeStore(state => state.recipes);
+
+  return (
+    <div>
+      {recipes.map(recipe => (
+        <div key={recipe.id}>
+          <h3>{recipe.title}</h3>
+          <p>{recipe.description}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
 
 
-export default useRecipeStore;
+export { RecipeList };
