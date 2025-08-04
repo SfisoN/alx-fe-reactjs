aafa045affe5a6a9e09a6fcbdf54d5e6b0c992a0
Sifiso Ntuli <sifisontulisn88@gmail.com>
2025-08-04 11:37:07 +0200

    commit update

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 
+// Fetch GitHub users based on advanced search criteria
 export async function fetchAdvancedUsers({ username, location, minRepos }) {
   let query = "";
 
@@ -16,4 +17,19 @@ export async function fetchAdvancedUsers({ username, location, minRepos }) {
 
   const response = await axios.get(url, { headers });
   return response.data; // Returns { total_count, items: [] }
+}
+
+// Fetch detailed GitHub user profile by username
+export async function fetchUserData(username) {
+  if (!username) throw new Error("Username is required to fetch user data");
+
+  const url = `https://api.github.com/users/${username}`;
+
+  const headers = {};
+  if (import.meta.env.VITE_APP_GITHUB_API_KEY) {
+    headers.Authorization = `token ${import.meta.env.VITE_APP_GITHUB_API_KEY}`;
+  }
+
+  const response = await axios.get(url, { headers });
+  return response.data; // Returns user profile object
 }
