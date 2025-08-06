1de04671ef177f28d60526d516f645aac74c23ab
Sifiso Ntuli <sifisontulisn88@gmail.com>
2025-08-06 08:41:15 +0200

    update commit

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -5,7 +5,7 @@ import doe from '../assets/hclogo.png'
 function UserProfile() {
   return (
     <div className='user-profile bg-gray-100 p-8 max-w-sm w-[400] mx-auto my-20 rounded-lg shadow-lg'>
-        <img src={doe} alt='User Avatar' className='avatar rounded-full w-40 h-40 mx-auto' />
+        <img src={doe} alt='User Avatar' className='avatar rounded-full w-36 h-36 mx-auto' />
         <h1 className='text-xl text-green-900 my-4'>John Doe</h1>
         <p className='text-gray-600 text-base'>Developer at Example Co. Loves to write code and explore new technologies.</p>
     </div>
