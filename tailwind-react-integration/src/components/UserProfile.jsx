e71c6c92b05fda860d11a2efa0aaea4d47910543
Sifiso Ntuli <sifisontulisn88@gmail.com>
2025-08-06 08:43:00 +0200

    new update

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -6,7 +6,7 @@ function UserProfile() {
   return (
     <div className='user-profile bg-gray-100 p-8 max-w-sm w-[400] mx-auto my-20 rounded-lg shadow-lg'>
         <img src={doe} alt='User Avatar' className='avatar rounded-full w-36 h-36 mx-auto' />
-        <h1 className='text-xl text-green-900 my-4'>John Doe</h1>
+        <h1 className='text-xl text-blue-800 my-4'>John Doe</h1>
         <p className='text-gray-600 text-base'>Developer at Example Co. Loves to write code and explore new technologies.</p>
     </div>
   )
