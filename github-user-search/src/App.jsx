97741055e259fd590062cece2deceffc36609144
Sifiso Ntuli <sifisontulisn88@gmail.com>
2025-08-09 17:02:35 +0200

    new update

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -1,6 +1,9 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
 import Home from "./components/Home";
 import About from "./components/About";
 import Search from "./components/Search";
+import "./App.css";
 
 function App() {
   return (
